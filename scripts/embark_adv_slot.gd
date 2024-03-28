837005d6e61c5c7b91db704b080e85150bf4e556
Lucas <lucasfschraier@gmail.com>
2024-03-28 15:01:00 -0400

    feat: disable interacting with adventurer row while that adventurer is occupied (dragged into embark slot)

--- scripts/embark_adv_slot.gd
+++ scripts/embark_adv_slot.gd
@@ -2,9 +2,22 @@ class_name EmbarkAdvSlot
 
 extends TextureRect
 
+
 func _can_drop_data(_pos, data:Variant) -> bool:
-	return data is Adventurer
-	
+	return is_valid_drop_data(data)
+
+
 func _drop_data(_pos, data:Variant) -> void:
-	if data is Adventurer:
-		texture = data.icon
+	texture = data.adv.icon
+	data.callback.call()
+
+
+func is_valid_drop_data(data:Variant) -> bool:
+	if data is Dictionary:
+		return (
+			data.has_all(["adv", "callback"]) 
+			and data.adv is Adventurer 
+			and data.callback is Callable
+		)
+	else:
+		return false
--- scripts/roster_row.gd
+++ scripts/roster_row.gd
@@ -1,27 +1,46 @@
 extends MarginContainer
 
 var adv:Adventurer
+var occupied := false
+
 
 func set_adventurer(adventurer:Adventurer):
 	adv = adventurer
 	set_text()
-	
+
+
 func set_text() -> void:
 	$HBoxContainer/VBoxContainer/AdventurerName.text = adv.adv_name
 	var info = "Level " + str(adv.level) + " " + adv.race + " " + adv.adv_class
 	$HBoxContainer/VBoxContainer/AdventurerInfo.text = info
-	
+
+
 func _get_drag_data(at_position: Vector2) -> Variant:
-	
-	var preview_texture = TextureRect.new()
-	preview_texture.texture = adv.icon
-	preview_texture.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
-	preview_texture.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
-	preview_texture.size = Vector2(48,48)
-	preview_texture.position = Vector2(-24,-24)
-	
-	var preview = Control.new()
-	preview.add_child(preview_texture)
-	set_drag_preview(preview)
-	
-	return adv
+	if not occupied:
+		var preview_texture = TextureRect.new()
+		preview_texture.texture = adv.icon
+		preview_texture.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+		preview_texture.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+		preview_texture.size = Vector2(48,48)
+		preview_texture.position = Vector2(-24,-24)
+		
+		var preview = Control.new()
+		preview.add_child(preview_texture)
+		set_drag_preview(preview)
+		
+		var res = {
+			"adv": adv,
+			"callback": end_drag
+		}
+		return res
+	else:
+		return null
+
+
+func end_drag()-> void:
+	occupy()
+
+
+func occupy()-> void:
+	occupied = true
+	$OccupiedPanel.visible = true
